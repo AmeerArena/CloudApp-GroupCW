fdb1e99d0ef254dc8f4bb9829d3ce6db770b0dd2
AmeerArena <ameerarena2000@gmail.com>
2025-12-22 16:26:07 +0300

    added the app.js and client.js

---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+//Set up express
+const express = require('express');
+const app = express();
+
+//Setup socket.io
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+//Setup static page handling
+app.set('view engine', 'ejs');
+app.use('/static', express.static('public'));
+
+//Handle client interface on /
+app.get('/', (req, res) => {
+  res.render('client');
+});
+//Handle display interface on /display
+app.get('/display', (req, res) => {
+  res.render('display');
+});
+
+// URL of the backend API
+const BACKEND_ENDPOINT = process.env.BACKEND || 'http://localhost:8181';
+
+//Start the server
+function startServer() {
+    const PORT = process.env.PORT || 8080;
+    server.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+}
+
+//Handle new connection
+io.on('connection', socket => { 
+  console.log('New connection');
+
+  //Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('Dropped connection');
+  });
+});
+
+//Start server
+if (module === require.main) {
+  startServer();
+}
+
+module.exports = server;
